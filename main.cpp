163ea4b99b8269d611a59262847fb1e4dfb27db0
苏日甫 <1020343499@qq.com>
2015-11-11 20:50:30 +0800

    指针与数组
    因为数组的名字就是其地址，指针p指向了数组a的起始地址，所以a[0]的地址与指针p的值一样，所以a[0]的值可以用p［0］来表示

--- main.cpp
+++ main.cpp
@@ -10,9 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int a,*p;
-    a=100;
-    p=&a;
-    printf("a=%d &a=%x &p=%x p=%x *p=%d \n",a,&a,&p,p,*p);
+    int a[4];
+    int *p;
+    a[0]=10;
+    a[1]=20;
+    a[2]=30;
+    a[3]=40;
+    p=a;
+    printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x p=%x \n",&a[0],&a[1],&a[2],&a[3],p);
+    printf("a[0]=%d \n",p[0]);
     return 0;
 }
